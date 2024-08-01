870d8770cf5f28943b342a47088f14591c5cc82b
DevEmperor <56255079+devemperor@users.noreply.github.com>
2024-08-01 21:31:39 +0200

    Version 2.1

--- app/src/main/java/net/devemperor/dictate/settings/DictateSettingsActivity.java
+++ app/src/main/java/net/devemperor/dictate/settings/DictateSettingsActivity.java
@@ -41,7 +41,8 @@ public class DictateSettingsActivity extends AppCompatActivity {
         } else if (sp.getInt("net.devemperor.dictate.last_version_code", 0) < BuildConfig.VERSION_CODE) {
             String whatsNewMessage = getString(R.string.dictate_changelog_donate);
             int lastVersionCode = sp.getInt("net.devemperor.dictate.last_version_code", 0);
-            if (lastVersionCode < 8) whatsNewMessage += getString(R.string.dictate_changelog_7);
+            if (lastVersionCode < 8) whatsNewMessage += getString(R.string.dictate_changelog_8);
+            if (lastVersionCode < 7) whatsNewMessage += getString(R.string.dictate_changelog_7);
             if (lastVersionCode < 6) whatsNewMessage += getString(R.string.dictate_changelog_6);
             if (lastVersionCode < 5) whatsNewMessage += getString(R.string.dictate_changelog_5);
             new MaterialAlertDialogBuilder(this)
