c92faa86c352885e2750ad99dd38da62d05864b8
Cuyler Stuwe <salembeats@gmail.com>
2025-04-12 17:31:39 -0700

    Update app/src/main/java/net/devemperor/dictate/settings/PreferencesFragment.java

--- app/src/main/java/net/devemperor/dictate/settings/PreferencesFragment.java
+++ app/src/main/java/net/devemperor/dictate/settings/PreferencesFragment.java
@@ -272,8 +272,6 @@ public class PreferencesFragment extends PreferenceFragmentCompat {
 
         Preference aboutPreference = findPreference("net.devemperor.dictate.about");
         if (aboutPreference != null) {
-            // asdf:wall
-
             aboutPreference.setTitle(getString(R.string.dictate_about, BuildConfig.VERSION_NAME));
             aboutPreference.setOnPreferenceClickListener(preference -> {
                 Toast.makeText(requireContext(), "User-ID: " + sp.getString("net.devemperor.dictate.user_id", "null"), Toast.LENGTH_LONG).show();
